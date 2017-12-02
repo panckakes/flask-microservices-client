096eaaa1ec0b496aacd7bb898a42d65323754ebc
Michael Diaz <panckakes@gmail.com>
2017-12-01 18:28:15 -0800

    fixed NavioTemperature.jsx

--- src/components/NavioTemperature.jsx
+++ src/components/NavioTemperature.jsx
@@ -4,9 +4,9 @@ const NavioTemperature = (props) => {
   return (
     <div>
       {
-        props.users.map((bluetooth) => {
+        props.users.map((naviotemperature) => {
           return <h4 key={ naviotemperature.id } className="well">
-            <strong>{ naviotemperature.temp }</strong> - 
+            <strong>{ naviotemperature.temp }</strong> -
             <em>{naviotemperature.created_at}</em></h4>
         })
       }
